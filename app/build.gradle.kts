602018c7ba986e6866c1d8f0daa8dcdc0bf2873d
KnowingGlows <knowingglows@gmail.com>
2024-06-28 18:08:02 +0545

    Shifting To Kotlin Build 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation ("androidx.recyclerview:recyclerview:1.3.2")
     implementation ("androidx.cardview:cardview:1.0.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     implementation ("com.android.billingclient:billing:6.2.1")
     implementation ("com.google.android.gms:play-services-ads:23.1.0")
